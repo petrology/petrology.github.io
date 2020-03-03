f1cb6e460a4ba48dcb25d79bb4d23379dc811581
maximfominykh <fominykh.ms@phystech.edu>
2020-03-03 16:29:07 +0300

    Add website files

---
+++ precache-manifest.9e88a18e989e2f8cecdea351261aa870.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = [
+  {
+    "revision": "a18abd2de764cc2c0e4c",
+    "url": "./static/css/main.47366d60.chunk.css"
+  },
+  {
+    "revision": "a18abd2de764cc2c0e4c",
+    "url": "./static/js/main.673a2874.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "d294726f9522143ff531",
+    "url": "./static/js/2.7eba3eca.chunk.js"
+  },
+  {
+    "revision": "3439b9b51cf76b689552e482f5eae397",
+    "url": "./static/media/zinov.3439b9b5.jpg"
+  },
+  {
+    "revision": "636d04bf2b0ab92feca37a752cf7508b",
+    "url": "./static/media/Parfenova.636d04bf.jpg"
+  },
+  {
+    "revision": "5c596078ad56777797704215607305e5",
+    "url": "./static/media/Shcherbakov.5c596078.jpg"
+  },
+  {
+    "revision": "28bdb17fd0661ed62601c55da8d83a2c",
+    "url": "./static/media/Sivorotkin.28bdb17f.jpg"
+  },
+  {
+    "revision": "202a13219515b5e4e6c5c8e57fb574d1",
+    "url": "./static/media/Sofia.202a1321.jpg"
+  },
+  {
+    "revision": "e5c696edcf1ed599ef6b4727dbdda452",
+    "url": "./static/media/alfereva.e5c696ed.jpg"
+  },
+  {
+    "revision": "96de32cc2881f2a04ed385cf51e8f980",
+    "url": "./static/media/ariskin.96de32cc.jpg"
+  },
+  {
+    "revision": "eaa6fadc82dfbd81b551af58e0a20c4c",
+    "url": "./static/media/bobrov.eaa6fadc.jpg"
+  },
+  {
+    "revision": "64ef84f8e1aef21a45f5d9e6c466f47c",
+    "url": "./static/media/glazovskay.64ef84f8.jpg"
+  },
+  {
+    "revision": "e8154b529e4325fd112f981d8d48a488",
+    "url": "./static/media/gram.e8154b52.jpg"
+  },
+  {
+    "revision": "e74009c694801ee4b24e5fd5eaae8fd6",
+    "url": "./static/media/gus.e74009c6.jpg"
+  },
+  {
+    "revision": "a8525108c5171fa46cac8fa145ee6a4e",
+    "url": "./static/media/korotaeva.a8525108.jpg"
+  },
+  {
+    "revision": "ff0608d6fbb12356a86bc391c16dceec",
+    "url": "./static/media/perchuk.ff0608d6.jpg"
+  },
+  {
+    "revision": "b73e621f318cd324716ad16ff8550567",
+    "url": "./static/media/plechov.b73e621f.jpg"
+  },
+  {
+    "revision": "f7a4806599ea1a848ff272eb8b9ca72b",
+    "url": "./static/media/podgornova.f7a48065.jpg"
+  },
+  {
+    "revision": "b67eb5c860e45808e3408c8c680dae23",
+    "url": "./static/media/safonov.b67eb5c8.jpg"
+  },
+  {
+    "revision": "e4e40fac6dfb2c599a1174f0224b006f",
+    "url": "./static/media/sazonova.e4e40fac.jpg"
+  },
+  {
+    "revision": "88cd085b3a98a6ca6cbba951bf99ea5c",
+    "url": "./static/media/schek.88cd085b.jpg"
+  },
+  {
+    "revision": "138a4f385055cab335990c16217a46e5",
+    "url": "./static/media/sharfman.138a4f38.jpg"
+  },
+  {
+    "revision": "7534d83190c09012266cfb1fbf4f9168",
+    "url": "./static/media/shkurskiy.7534d831.jpg"
+  },
+  {
+    "revision": "d0c791dce60282e1ab2eafbed3cee384",
+    "url": "./static/media/shur.d0c791dc.jpg"
+  },
+  {
+    "revision": "79f100e5d77ab2c0ef6867d5b13ce916",
+    "url": "./static/media/taras.79f100e5.jpg"
+  },
+  {
+    "revision": "f76aa500a78433a0c93432497ccf6475",
+    "url": "./static/media/vasya.f76aa500.jpg"
+  },
+  {
+    "revision": "3cb0dccb117313f33aec31341b992b65",
+    "url": "./static/media/aboutlab2.3cb0dccb.jpg"
+  },
+  {
+    "revision": "62600e81ee3aae4b21a8424ebd6fe21a",
+    "url": "./static/media/aboutlab2_2.62600e81.jpg"
+  },
+  {
+    "revision": "f75a7d8a916a9eb78c3157fe82c41995",
+    "url": "./static/media/aboutlab2_3.f75a7d8a.jpg"
+  },
+  {
+    "revision": "2f1923cbdd377d79ba34ec757aeb6859",
+    "url": "./static/media/aboutlab2_9.2f1923cb.jpg"
+  },
+  {
+    "revision": "faf19718800da935764dacbc51934e7b",
+    "url": "./static/media/aboutlab3_2.faf19718.jpg"
+  },
+  {
+    "revision": "d9c14f075ebf4fd4791f33a3f7751f31",
+    "url": "./static/media/lab1.d9c14f07.jpg"
+  },
+  {
+    "revision": "62600e81ee3aae4b21a8424ebd6fe21a",
+    "url": "./static/media/lab2.62600e81.jpg"
+  },
+  {
+    "revision": "e865ae8a40fb9f253deaaae96b49f46b",
+    "url": "./static/media/lab3.e865ae8a.jpg"
+  },
+  {
+    "revision": "03b6029de64d6985c67310f979525911",
+    "url": "./static/media/kamchtka.03b6029d.jpg"
+  },
+  {
+    "revision": "0dd4ddd329ca45ed5d73f604a9846fdd",
+    "url": "./static/media/meteorit.0dd4ddd3.jpg"
+  },
+  {
+    "revision": "c293d87a17096780e5914d2ec5d29d18",
+    "url": "./static/media/camping1.c293d87a.jpeg"
+  },
+  {
+    "revision": "1acc7045355a4b293828e26b79d3baa9",
+    "url": "./static/media/camping2.1acc7045.jpeg"
+  },
+  {
+    "revision": "e300740bf337fbaee6f334a092659107",
+    "url": "./static/media/hist1.e300740b.jpg"
+  },
+  {
+    "revision": "36f4b9d96c4a3e8b97261143150b2d2a",
+    "url": "./static/media/hist2.36f4b9d9.jpg"
+  },
+  {
+    "revision": "18f3692540c5f082cd1a7217f849560c",
+    "url": "./static/media/courseworkFirstYear.18f36925.pdf"
+  },
+  {
+    "revision": "1710e86ef33405d1860982c2173ba43d",
+    "url": "./static/media/slice.1710e86e.jpg"
+  },
+  {
+    "revision": "6edeea0e8914b34dfb65d9886df920ea",
+    "url": "./static/media/abitu.6edeea0e.jpg"
+  },
+  {
+    "revision": "f0229f6c647c7532a20f83c198ee84a8",
+    "url": "./static/media/students.f0229f6c.jpg"
+  },
+  {
+    "revision": "23467a0de5b5722b80a405e3605907b3",
+    "url": "./static/media/smoked.23467a0d.jpg"
+  },
+  {
+    "revision": "a290e0d460ce93ca16e30c7ac8b02a67",
+    "url": "./static/media/mountains.a290e0d4.jpg"
+  },
+  {
+    "revision": "5306b2605974d573927a8629fa9c914e",
+    "url": "./static/media/eruption.5306b260.jpg"
+  },
+  {
+    "revision": "edfdf660fd41c6011d3b407b2825dcf4",
+    "url": "./static/media/nosova.edfdf660.jpg"
+  },
+  {
+    "revision": "000dea76154507afcdfff179c410cafd",
+    "url": "./index.html"
+  }
+];
